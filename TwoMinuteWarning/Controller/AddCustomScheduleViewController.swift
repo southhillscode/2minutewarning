6034d325660a0b2bca64eeae65172a52894a480f
Zach Zhong <zzy@Zachs-MacBook-Air.local>
2017-10-19 02:28:29 -0700

    Added new ViewController that allowed user to put in custom schedule; No code written yetl

---
+++ TwoMinuteWarning/Controller/AddCustomScheduleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddCustomScheduleViewController.swift
+//  TwoMinuteWarning
+//
+//  Created by Zach Zhong on 19/10/2017.
+//  Copyright © 2017 Rob Fitzgerald, Inc. All rights reserved.
+//
+
+import UIKit
+
+class AddCustomScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func dismiss(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
